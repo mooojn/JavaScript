f628e82a036f82747b677e7b4a2dd2535e6587d2
Munees Tariq <moontariq7867@gmail.com>
2023-12-03 15:45:30 +0500

    variableTypes

---
+++ variable.js
@@ -0,0 +1,9 @@
+/// variables 
+let firstName = 'Munees';       
+let lastName = 'Tariq';         
+const age = 21;              /// constant can't change its value     
+let employmentStatus = false;
+//// output
+console.log("Name: " + firstName + " " + lastName);
+console.log("Age: " + age);
+console.log("Currently Employed: " + employmentStatus);
